4bc78b49be54e0194b4df8a5b7e3695614745464
David <bittneradave@gmail.com>
2015-11-05 21:55:00 -0500

    Adding bulles

---
+++ bullet.cpp
@@ -0,0 +1,65 @@
+#ifndef BULLET_CPP
+#define BULLET_CPP
+
+#include "coord.cpp"
+#include "glfw_funcs.cpp"
+
+//Bullet class for handling their movement/drawing
+class Bullet
+{
+	
+	public:
+		Bullet( Coord ply, float ang, float vel, float decel );
+
+		void Move();
+		void Draw();
+
+	private:
+
+		float xvel, yvel;
+		float decel;
+		Coord pos;
+
+};
+
+Bullet::Bullet( Coord ply, float ang, float vel, float decel ) : pos( 0, 0 )
+{
+
+	xvel = cos( ang )*vel;
+	yvel = sin( ang )*vel;
+
+	pos.x = ply.x;
+	pos.y = ply.y;
+
+	this->decel = decel;
+
+}
+
+void Bullet::Move()
+{
+
+	float decelAng = GetInclin( Coord( 0, 0 ), pos );
+
+	float xPull = cos(decelAng)*(decel/6);
+	float yPull = sin(decelAng)*(decel/6);
+
+	xvel-=xPull;
+	yvel-=yPull;
+
+	pos.x+=xvel;
+	pos.y+=yvel;
+
+}
+
+void Bullet::Draw()
+{
+
+	glPointSize( 15 );
+
+	glBegin( GL_POINTS );
+		glVertex2f( pos.x, pos.y );
+	glEnd();
+
+}
+
+#endif
